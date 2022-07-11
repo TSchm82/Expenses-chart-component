b3f446ec441e5cc0da87157fd92030d42b9ff431
Tobias Schmidt <tobias.schmidt@shapefield.de>
2022-07-11 12:00:27 +0200

    cleaned up code

--- expenses-chart-component-main/fetchData.js
+++ expenses-chart-component-main/fetchData.js
@@ -24,43 +24,52 @@ function createBars(spendings) {
     const container = document.getElementById("graphs-container")
     const maximumHeight = findMaximum(spendings.map(e => e.amount));
 
+    // Create bar and add it to container
     spendings.forEach((spending, index) => {
-        barContainer = document.createElement('div');
-        barContainer.classList.add('bar-container');
+        barContainer = createElementAndAddClass('div', 'bar-container');
 
+        // Add space on the right
         if (index !== spendings.length - 1) {
             barContainer.classList.add('bar-spacer-right');
         }
 
+        // Create Elements
+        infoBox = createElementAndAddClass('div', 'info-box', `$ ${spending.amount}`);
+        spacerTop = createElementAndAddClass('div', 'bar-spacer-top');
+        info = createElementAndAddClass('span', 'column-info-day', spending.day);
+        bar = createElementAndAddClass('div', 'bar');
+
+        // Add Styling
         const barHeight = spending.amount;
+        bar.style.height = `${70 * barHeight / maximumHeight}%`;
+        spacerTop.style.height = `${70 * (maximumHeight - barHeight) / maximumHeight}%`;
 
-        infoSpace = document.createElement('div');
-        infoSpace.classList.add('info-spacer');
-        infoSpace.innerHTML = `$ ${spending.amount}`;
-
-        spacer = document.createElement('div');
-        spacer.classList.add('bar-spacer-top');
-        spacer.style.height = `${70 * (maximumHeight - barHeight) / maximumHeight}%`;
-
-        bar = document.createElement('div');
-        bar.classList.add('bar');
-
+        // Style Maximum
         if (barHeight === maximumHeight) {
             bar.classList.add('maximum');
         }
 
-        bar.style.height = `${70 * barHeight / maximumHeight}%`;
+        console.log(bar)
 
-        info = document.createElement('span');
-        info.innerHTML = spending.day;
-
-        barContainer.appendChild(spacer);
-        barContainer.appendChild(infoSpace);
+        // Append to container
+        barContainer.appendChild(spacerTop);
+        barContainer.appendChild(infoBox);
         barContainer.appendChild(bar);
         barContainer.appendChild(info);
 
         container.appendChild(barContainer)
     })
+}
+
+function createElementAndAddClass(type, style, innerHTML) {
+    element = document.createElement(type);
+    element.classList.add(style);
+
+    if (!!innerHTML) {
+        element.innerHTML = innerHTML;
+    }
+    console.log(element)
+    return element;
 }
 
 async function fetchData() {
